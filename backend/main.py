7162bba8c734844cbaaa9f95025bc52ccf85c30d
Toni <toni@kukec.dev>
2025-10-15 00:29:22 +0200

    working basic auth
    bez onog sa 2 vrste usera zasad

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,46 @@
-from typing import Union
-from fastapi import FastAPI
-app = FastAPI()
+from contextlib import asynccontextmanager
+
+from fastapi import Depends, FastAPI
+
+from modules.db import User, create_db_and_tables
+from modules.schemas import UserCreate, UserRead, UserUpdate
+from modules.users import auth_backend, current_active_user, fastapi_users
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Not needed if you setup a migration system like Alembic
+    await create_db_and_tables()
+    yield
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+
+
+@app.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
