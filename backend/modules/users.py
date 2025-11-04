cb2eed4ab6d64c181ac121e13fb34f79e6c48cbd
Tonik <toni@kukec.dev>
2025-11-04 01:19:17 +0100

    Revert "Make oauth response redirect"
    This reverts commit cefdeb6c5782cf217d47ff03460c9f8e691e1612.

--- backend/modules/users.py
+++ backend/modules/users.py
@@ -5,13 +5,12 @@ from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, FastAPIUsers, UUIDIDMixin, models
 from fastapi_users.authentication import (
     AuthenticationBackend,
-    CookieTransport,
+    BearerTransport,
     JWTStrategy,
 )
 from fastapi_users.db import SQLAlchemyUserDatabase
 from httpx_oauth.clients.google import GoogleOAuth2
 from modules.db import User, get_user_db
-from modules.my_oauth_transport import RedirectCookieTransport
 import os
 
 SECRET = os.getenv("AUTH_KEY")
@@ -45,9 +44,7 @@ async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db
     yield UserManager(user_db)
 
 
-cookie_transport = RedirectCookieTransport(
-    cookie_name="session", login_redirect_url="http://localhost:3000",
-    logout_redirect_url="http://localhost:3000", cookie_secure=False, cookie_samesite="lax", cookie_domain=None)
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
 
 def get_jwt_strategy() -> JWTStrategy[models.UP, models.ID]:
@@ -56,7 +53,7 @@ def get_jwt_strategy() -> JWTStrategy[models.UP, models.ID]:
 
 auth_backend = AuthenticationBackend(
     name="jwt",
-    transport=cookie_transport,
+    transport=bearer_transport,
     get_strategy=get_jwt_strategy,
 )
 
